d60ec97817ef98bd845c17038ea00d381920093a
Simon Bennett <simon@traxsound.co.uk>
2019-02-18 20:14:05 +0000

    add linear layout

---
+++ app/src/main/java/com/alexsamazingapps/ollie30gameandroid/GameActivity.kt
@@ -0,0 +1,12 @@
+package com.alexsamazingapps.ollie30gameandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game_activity)
+    }
+}

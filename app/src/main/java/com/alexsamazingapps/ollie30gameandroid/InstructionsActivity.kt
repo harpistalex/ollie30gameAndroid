d6771cec8537940cc329f90410ccc07f75cfbf82
Simon Bennett <simon@traxsound.co.uk>
2019-02-19 09:32:11 +0000

    add 3 screens

---
+++ app/src/main/java/com/alexsamazingapps/ollie30gameandroid/InstructionsActivity.kt
@@ -0,0 +1,12 @@
+package com.alexsamazingapps.ollie30gameandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InstructionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.instructions_activity)
+    }
+}
